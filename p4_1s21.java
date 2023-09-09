9032382ffb9396d5c11b00e87b86461bd493b4b4
JohanRacielSalgadoAguilar <144179211+JohanRacielSalgadoAguilar@users.noreply.github.com>
2023-09-08 22:37:14 -0600

    1s21
    Salgado Aguilar Johan Raciel  p3_1s21 y p4_1s21

---
+++ p4_1s21.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class p3_1s22{
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String nombre;
+		System.out.println("ingresa tu nombre");
+		nombre = teclado.nextLine(); 
+
+
+		Scanner años=new ClassCastException(System.in);
+		Staring edad;
+		System.out.println("ingresa tu edad");
+	    edad = teclado nextLine(); 
+
+
+	    scanner email = new Scanner(System.in);
+	    Staring correo;
+	    System.out.println("Ingresa tu correo");
+	    correo = teclado.nextLine();
+
+
+	    Scanner celular = new Scanner(System.in);
+	    String telefono;
+	    System.out.println("Ingresa tu nombre");
+	    telefono = teclado.nextline();
+	    System.out.println("Bienvenido" + nombre);
+	    System.out.println("Tu edad es" + edad);
+	    System.out.println("tu correo es" + correo);
+	    System.out.println("Tu numero es" + telfono)
+
+
+
+ 	
+	}
+}
