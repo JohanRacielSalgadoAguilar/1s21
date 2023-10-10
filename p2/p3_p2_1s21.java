9354a9dc7cdc87be1ea30347234e30029cbe4b4f
JohanRacielSalgadoAguilar <144179211+JohanRacielSalgadoAguilar@users.noreply.github.com>
2023-10-10 15:05:40 -0600

    Add files via upload

---
+++ p2/p3_p2_1s21.java
@@ -0,0 +1,9 @@
+public class p3_p2_1s21{
+	public static void main(String[] args) {
+		int a,b;
+		for(a=0;a<11;a++){
+			for(b=1;b<11;b++)
+				System.out.println(a+"x"+b+"="+(a*b));
+		}	
+	}
+}
---
+++ p2/p4p2_1s21.java
@@ -0,0 +1,18 @@
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        System.out.println("Números primos del 1 al 10:");
+        
+        for (int i = 2; i <= 10; i++) {
+            boolean esPrimo = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+            if (esPrimo) {
+                System.out.println(i);
+            }
+        }
+    }
+}
