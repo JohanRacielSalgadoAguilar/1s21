608ff12b6ec9eaf71ab8c6b9d8c90e9b011a6cf8
JohanRacielSalgadoAguilar <144179211+JohanRacielSalgadoAguilar@users.noreply.github.com>
2023-10-10 14:37:03 -0600

    Add files via upload

---
+++ p1/p2_1s21.java
@@ -0,0 +1,10 @@
+class p2_1s21{
+	public static void mian(String[] args) {
+		;System.out.printl("datos personales")
+		;Edad.printl("18")
+		;Correo.printl("johanraciel@gmail.com")
+		;Nombre.printl("salgado aguilar johan raciel")
+		;Aficion.printl("estudiante")
+		Colorfavorito.printl("azul")
+	}
+}
---
+++ p1/p9_1s21 (1).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class p9_1s21{
+	public static void main(String[] args) {
+		scanner teclado = new Scanner(System.in);
+		System operacion;
+		System.out.println("Que tipo de operacion desea restar");
+		System.out.System.out.print("escribala: Suma, Resta");
+		operacion = teclado.nextline();
+
+
+		if (operacion == "suma"){
+			System.out.println("Haremos una suma");
+
+		}
+
+		if (operacion == "Resta"){
+			System.out.println("Haremos una suma");
+
+		}
+
+	}
+}
